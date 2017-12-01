5e6b283e685c36c4cbfc73052a1301380249c757
thiminhnhut <thiminhnhut@gmail.com>
2017-12-01 09:51:44 +0700

    Test thay doi man hinh chinh

---
+++ examples/TestScreen/app/src/main/java/com/desktop/minhnhut/testscreen/Screen2Activity.kt
@@ -0,0 +1,12 @@
+package com.desktop.minhnhut.testscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Screen2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen2)
+    }
+}
